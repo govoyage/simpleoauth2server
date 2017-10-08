767fe3be36f1856ab5d38dfbed9865467af4ae13
hpmtissera <hpmtissera@gmail.com>
2017-10-08 19:04:42 +0900

    fixed #3

--- src/main/java/com/lanka_guide/simpleoauth2server/OAuthConfiguration.java
+++ src/main/java/com/lanka_guide/simpleoauth2server/OAuthConfiguration.java
@@ -3,6 +3,7 @@ package com.lanka_guide.simpleoauth2server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +12,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
 
@@ -31,7 +34,12 @@ public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
                 .withClient("acme")
                 .secret("acmesecret")
                 .autoApprove(true)
-                .authorizedGrantTypes("password").scopes("read,write").autoApprove(".*");
+                .authorizedGrantTypes("password").scopes("read,write");
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
     }
 
     @Override
@@ -43,6 +51,6 @@ public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore());
     }
 }
