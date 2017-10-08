9354cb0619f10d03149681988b0f2d5f13c64056
hpmtissera <hpmtissera@gmail.com>
2017-10-08 17:52:54 +0900

    towards #3

---
+++ src/main/java/com/lanka_guide/simpleoauth2server/OAuthConfiguration.java
@@ -0,0 +1,48 @@
+package com.lanka_guide.simpleoauth2server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+import javax.sql.DataSource;
+
+@EnableAuthorizationServer
+@Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("acme")
+                .secret("acmesecret")
+                .autoApprove(true)
+                .authorizedGrantTypes("password").scopes("read,write").autoApprove(".*");
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        oauthServer
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+}
